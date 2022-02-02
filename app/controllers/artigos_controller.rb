66c4b09764d3d5d23235cbbaf636a1603d4f5158
Carlos Junior Cordeiro da Silva <carlos.dasilva@goias.gov.br>
2022-02-02 17:06:44 -0300

    Criado actions para criar artigo; criado view para preencher dados do artigo e exibir; feito validações dos atributos do artigo

--- app/controllers/artigos_controller.rb
+++ app/controllers/artigos_controller.rb
@@ -4,7 +4,28 @@ class ArtigosController < ApplicationController
   end
 
   def show
-    byebug
     @artigo = Artigo.find(params[:id])    
   end
+
+  def create
+    @artigo = Artigo.new(artigos_params)
+
+    if @artigo.save
+      # direciona para o show do artigo, o rails entende isso
+      redirect_to @artigo
+    else
+      render :new
+    end
+  end
+
+  def new
+    @artigo = Artigo.new
+  end
+
+  private
+
+  def artigos_params
+    params.require(:artigo).permit(:titulo, :conteudo)
+  end
+
 end
--- app/models/artigo.rb
+++ app/models/artigo.rb
@@ -1,3 +1,6 @@
 class Artigo < ApplicationRecord
+    validates :titulo, presence: true, length: { minimum: 5 }
+    validates :conteudo, presence: true, length: { minimum: 10}
+
     self.table_name = 'artigos'
 end
